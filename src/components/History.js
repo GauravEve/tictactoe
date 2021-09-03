90e2ced802e9b91073bccdac90438edd195e7fae
gaurav <gaurav.1si19cs042@gmail.com>
2021-09-03 21:16:35 +0530

    styled the entire app

--- src/components/History.js
+++ src/components/History.js
@@ -2,15 +2,14 @@ import React from 'react'
 
 const History = ({history,moveTo,currentMove}) => {
     return (
-        <ul>
+        <div className="history-wrapper">
+        <ul className="history">
             {
                 history.map((_, move)=>{
            return(
                 <li key={move}>
                 <button 
-                style={{
-                    fontWeight:move===currentMove?'bold':'normal',
-                }}
+                className={`btn-move ${move==currentMove?'active':''}`}
                 type="button" 
                 onClick={()=>{
                     moveTo(move);
@@ -19,9 +18,11 @@ const History = ({history,moveTo,currentMove}) => {
                     </button>
                 </li>
            );
+           
             })}
             
         </ul>
+        </div>
     );
 };
 
